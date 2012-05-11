642f725a19dcbab7ce69aa190acbca702b231eff
Amit Chembra <amit.bhavadas@gmail.com>
2012-05-10 21:13:37 -0500

    Posting the code
    JS file which actually parses the url and does an http post.

--- README.md
+++ README.md
@@ -2,3 +2,21 @@ JumbalakkaLinkPost
 ==================
 
 Quick dirty way to do HTTP POST from links on your page
+
+This should be helpful to users who have tons of anchor tag on their 
+webpage and need they need to do a HTTP post for the same. 
+
+One use case I found is helpful is in when display tags are used in your
+page. There should be a way to override display tag to secure the code 
+even more. But I wanted to put a simpler solution out there.
+
+Usage is fairly simple. 
+
+$('a').click(function(evnt) {
+    evnt.preventDefault();
+    new JumbalakkaLinkPost($(this).attr('href')).doPost();
+});​
+
+The code is in pure JS but I am using Jquery here to demonstrate the usage. 
+http://jsfiddle.net/MAsP7/9/
+
---
+++ src/jumbalakkaLinkPost.js
@@ -0,0 +1,38 @@
+/**
+ * Author: 		Amit B Chembra
+ * Use Case: 	When all your links are in get form and you need to convert them 
+ * 			 	into post. This is a quick dirty way of achieving that. 
+ * 
+ * Simple function to do a post from a get type url
+ * ref: http://papermashup.com/read-url-get-variables-withjavascript/
+ * 
+ * e.g.; http://jsfiddle.net/MAsP7/9/
+ */
+function JumbalakkaLinkPost( url ) 
+{
+    var params = {};
+    //regex to parse the parameter in key/value pair separately
+    var parts = url.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+    	params[key] = value;
+    });
+    this.parameters = params;
+    this.link = url.split("?")[0];
+}
+
+JumbalakkaLinkPost.prototype.doPost = function()
+{
+	var formContent = document.createElement( 'form' );
+	
+	formContent.action = this.link;
+	
+	for( var key in this.parameters )
+	{
+		formContent.innerHTML += "<input type='hidden' name='" + key + "' " +
+						"value='" + this.parameters[key] + "'/>"
+	}
+	
+	document.getElementsByTagName( "body" )[0]
+			.appendChild( formContent );
+	
+	formContent.submit();
+}
